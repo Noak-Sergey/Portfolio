060d091367f962ac6fb216d220f86eff4d812e96
Noak-Sergey <noakstuff@gmail.com>
2021-12-15 21:19:55 +0300

    feat: added style

---
+++ src/common/components/title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from './Title.module.css';
+
+export function Title(props) {
+    return (
+        <div className={style.title}>
+            <h2>{props.text}</h2>
+        </div>
+    )
+}
--- src/works/Works.js
+++ src/works/Works.js
@@ -2,15 +2,14 @@ import React from "react";
 import style from './Works.module.css';
 import styleContainer from '../common/styles/Container.module.css'
 import {Work} from "./work/Work";
+import {Title} from "../common/components/title/Title";
 
 
 export function Works() {
     return (
         <div className={style.worksBlock}>
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
-                <div className={style.title}>
-                    <h2 >My works</h2>
-                </div>
+                <Title text={'My works'}/>
                 <div className={style.works}>
                     <Work title={'Todolist'} description={'Project description'} />
                     <Work title={'Social-Network'} description={'Project description'}/>
