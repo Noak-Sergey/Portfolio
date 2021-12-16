41793818c03ad068bbac7b5b8126ea98be66a7a1
Noak-Sergey <noakstuff@gmail.com>
2021-12-16 22:09:08 +0300

    feat: added style and images

--- src/works/Works.js
+++ src/works/Works.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React from 'react';
 import style from './Works.module.css';
 import styleContainer from '../common/styles/Container.module.css'
-import {Work} from "./work/Work";
-import {Title} from "../common/components/title/Title";
+import {Work} from './work/Work';
+import {Title} from '../common/components/title/Title';
+import todoImg from '../assets/images/todo.jpg';
+import socialImg from '../assets/images/social_network.jpg';
 
 
 export function Works() {
+    const social = {
+        backgroundImage: `url(${socialImg})`,
+    }
+
+    const todos = {
+        backgroundImage: `url(${todoImg})`,
+    }
+
     return (
         <div className={style.worksBlock}>
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
                 <Title text={'My works'}/>
                 <div className={style.works}>
-                    <Work title={'Todolist'} description={'Project description'} />
-                    <Work title={'Social-Network'} description={'Project description'}/>
+                    <Work style={todos} title={'Todolist'} description={'Project description'} />
+                    <Work style={social} title={'Social-Network'} description={'Project description'}/>
                 </div>
             </div>
         </div>
--- src/works/work/Work.js
+++ src/works/work/Work.js
@@ -5,14 +5,16 @@ import style from './Work.module.css';
 export function Work(props) {
     return (
         <div className={style.work}>
-            <div className={style.imgContainer}>
-                <h6>Picture</h6>
+            <div className={style.image} style={props.style}>
                 <button className={style.viewBtn}>look</button>
             </div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>
+            <div className={style.workInfo}>
+                <h3 className={style.workTitle}>{props.title}</h3>
+                <span className={style.description}>
                 {props.description}
             </span>
+            </div>
+
         </div>
     )
 }
