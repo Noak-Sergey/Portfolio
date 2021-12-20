64e4100a16b5abf1af10a59f43104aea63ccc6a0
Noak-Sergey <noakstuff@gmail.com>
2021-12-20 23:00:49 +0300

    ref: refactor Skills, added images

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,18 +3,45 @@ import style from './Skills.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
 import {Title} from "../common/components/title/Title";
+import reactReduxImg from "../assets/images/react_redux.png";
+import jsTsImg from "../assets/images/jsTs.jpg";
+import htmlCssImg from "../assets/images/htmlCss.jpg";
+import axiosRestImg from "../assets/images/axios.png";
+import gitGithubImg from "../assets/images/gitGithub.png";
 
 
 export function Skills() {
+
+    const reactRedux = {
+        backgroundImage: `url(${reactReduxImg})`,
+    }
+
+    const jsTs = {
+        backgroundImage: `url(${jsTsImg})`,
+    }
+
+    const htmlCss = {
+        backgroundImage: `url(${htmlCssImg})`,
+    }
+
+    const axiosRest = {
+        backgroundImage: `url(${axiosRestImg})`,
+    }
+
+    const gitGithub = {
+        backgroundImage: `url(${gitGithubImg})`,
+    }
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill title={'React & Redux'} description={'It is cool'}/>
-                    <Skill title={'JS & TS'} description={'It is really cool'}/>
-                    <Skill title={'HTML & CSS'} description={'bla bla bla'}/>
-                    <Skill title={'Free'} description={'yo YO Yo'}/>
+                    <Skill style={reactRedux} title={'React & Redux'} description={'It is cool'}/>
+                    <Skill style={jsTs} title={'JS & TS'} description={'It is really cool'}/>
+                    <Skill style={htmlCss} title={'HTML & CSS'} description={'bla bla bla'}/>
+                    <Skill style={axiosRest} title={'Axios & REST API'} description={'yo YO Yo'}/>
+                    <Skill style={gitGithub} title={'Git & Github'} description={'repository'}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,16 +1,16 @@
 import React from "react";
-import style from './Skill.module.css';
-
-
+import style from './Skill.module.scss';
 
 export function Skill(props) {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>
+            <div className={style.icon} style={props.style}></div>
+            <div className={style.skillInfo}>
+                <h3>{props.title}</h3>
+                <span className={style.description}>
                 {props.description}
             </span>
+            </div>
         </div>
     )
 }
