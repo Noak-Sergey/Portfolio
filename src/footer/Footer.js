18a6e8b113db1d20955f7505dbf205a48b43a5ee
Noak-Sergey <noakstuff@gmail.com>
2021-12-27 18:37:18 +0300

    feat: added links to footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -7,42 +7,30 @@ import githubImg from "../assets/images/github-logo.png";
 import codewarsImg from "../assets/images/codewars_logo.png";
 
 
-
 export function Footer() {
-
-    const linkedin = {
-        backgroundImage: `url(${linkedinImg})`,
-    }
-
-    const telegram = {
-        backgroundImage: `url(${telegramImg})`,
-    }
-
-    const github = {
-        backgroundImage: `url(${githubImg})`,
-    }
-
-    const codewars = {
-        backgroundImage: `url(${codewarsImg})`,
-    }
 
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Footer</h2>
+
                 <div className={style.footers}>
-                    <div style={linkedin} className={style.footerIcon}>
-                        <a href=''> </a>
-                    </div>
-                    <div style={telegram} className={style.footerIcon}>
-                        <a href=''> </a>
-                    </div>
-                    <div style={github} className={style.footerIcon}>
-                        <a href=''> </a>
-                    </div>
-                    <div style={codewars} className={style.footerIcon}>
-                        <a href=''> </a>
-                    </div>
+
+                        <a href='https://www.linkedin.com/in/sergey-noak-065503220/'>
+                            <img className={style.footerIcon} src={linkedinImg} alt='linkedin'/>
+                        </a>
+
+                        <a href='https://t.me/NoakSergey'>
+                            <img className={style.footerIcon} src={telegramImg} alt='telegram'/>
+                        </a>
+
+                        <a href='https://github.com/Noak-Sergey'>
+                            <img className={style.footerIcon} src={githubImg} alt='github'/>
+                        </a>
+
+                        <a href='https://www.codewars.com/users/Sergey%20Noak'>
+                            <img className={style.footerIcon} src={codewarsImg} alt='codewars'/>
+                        </a>
+
                 </div>
                 <div className={style.copyright}>
                     © 2021 Sergey Noak. All rights reserved.
