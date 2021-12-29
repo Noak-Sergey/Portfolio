6e02f93d3ad5363be648659c06c78bdeb618aa20
Noak-Sergey <noakstuff@gmail.com>
2021-12-29 10:26:49 +0300

    feat: added effects for myPhoto

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,22 +3,33 @@ import style from './Main.module.scss';
 import styleContainer from './../common/styles/Container.module.css';
 import myPhoto from '../assets/images/photoMy.jpg'
 import Fade from 'react-reveal/Fade';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-tilt'
 
 export function Main() {
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
-                <Fade left>
-                    <div className={style.hiText}>
-                        <span>Hi There</span>
-                        <h1>I am Sergey <span>Noak</span></h1>
-                        <p>Frontend Developer</p>
-                    </div>
-                </Fade>
+
+                <div className={style.hiText}>
+                    <span>Hi There</span>
+                    <h1>I am Sergey <span>Noak</span></h1>
+                    <ReactTypingEffect
+                        text={["Frontend Developer"]}
+                        speed={300}
+                        eraseDelay={2500}
+                        typingDelay={1500}
+                    />
+                </div>
+
                 <Fade right>
-                    <div>
-                        <img src={myPhoto} className={style.photo} alt='My photo'/>
-                    </div>
+                    <Tilt className="Tilt" options={{max: 20}}>
+                        <div>
+                            <img src={myPhoto} className={style.photo} alt='My photo'/>
+                        </div>
+                    </Tilt>
+
+
                 </Fade>
             </div>
 
