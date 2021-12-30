9e9d301b07fa5a14c7e00d4631aafe2a663818aa
Noak-Sergey <noakstuff@gmail.com>
2021-12-30 17:27:42 +0300

    feat: added BurgerNav

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import style from './Header.module.scss';
 import {Nav} from "./nav/Nav";
 import Fade from 'react-reveal/Fade';
+import {BurgerNav} from "./burgerNav/BurgerNav";
 
 export function Header() {
     return (
         <div className={style.header}>
             <Fade top>
                 <Nav/>
+                <BurgerNav/>
             </Fade>
         </div>
     )
