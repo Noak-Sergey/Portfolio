bc1c3ef7c4769df063e9bb83bc979eda54cdcbcb
Noak-Sergey <noakstuff@gmail.com>
2022-01-05 18:04:34 +0300

    feat: added BurgerNav2 and icon menu

--- src/header/burgerNav/BurgerNav.js
+++ src/header/burgerNav/BurgerNav.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './BurgerNav.module.scss';
 import {Link, animateScroll as scroll} from "react-scroll";
+import burgerMenuIcon from "../../assets/images/burgerMenu2.png";
 
 export function BurgerNav() {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    const onBurgerBtnClick = () => {
+        setMenuIsOpen(!menuIsOpen);
+    }
+
     return (
         <div className={style.burgerNav}>
-            <div className={style.burgerNavItems}>
+            <div className={ menuIsOpen ? `${style.burgerNavItems} ${style.show}` : style.burgerNavItems}>
                 <Link activeClass={style.active} to="main" spy={true} smooth={true} offset={-80}
                       duration={500}>Main</Link>
                 <Link activeClass={style.active} to="skills" spy={true} smooth={true} offset={-80}
@@ -14,6 +21,9 @@ export function BurgerNav() {
                       duration={500}>Projects</Link>
                 <Link activeClass={style.active} to="contacts" spy={true} smooth={true} offset={-80}
                       duration={500}>Contacts</Link>
+            </div>
+            <div onClick={onBurgerBtnClick} className={style.burgerBtn}>
+                <img className={style.burgerIcon} src={burgerMenuIcon} alt='menu'/>
             </div>
         </div>
     )
