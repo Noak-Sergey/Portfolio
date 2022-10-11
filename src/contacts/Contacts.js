494a723a39af3390d2f8fbb17ac7e0e14ce67ed7
Noak-Sergey <noakstuff@gmail.com>
2022-10-11 13:56:11 +0300

    feat: fixed send message form to my email

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Fade from 'react-reveal/Fade';
+import emailjs from 'emailjs-com';
 
 import {Title} from "../common/components/title/Title";
 
@@ -7,9 +8,22 @@ import style from './Contacts.module.scss';
 import styleContainer from './../common/styles/Container.module.css';
 
 
-
-
 export function Contacts() {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_bttze3h', 'template_ia85hol', e.target, 'HFzDXtRnoFAqeXj98')
+        .then(res => {
+            console.log(res);
+            alert('Message was send !!!');
+        })
+        .catch(err => {
+            console.log(err);
+            alert('Something went wrong !!!');
+        });
+    };
+
     return (
         <div id='contacts' className={styleContainer.container}>
             <div className={style.contactBlock}>
@@ -22,11 +36,11 @@ export function Contacts() {
 
                 <Fade right>
                     <div className={style.form_wrapper}>
-                        <form>
+                        <form onSubmit={sendEmail}>
                             <ul>
                                 <li className={style.form_line}>
                                     <label htmlFor="name">Name</label>
-                                    <input type="text" id="name" required/>
+                                    <input type="text" id="name" name="name" required/>
                                 </li>
                                 <li className={style.form_line}>
                                     <label htmlFor="phone">Phone</label>
@@ -34,11 +48,11 @@ export function Contacts() {
                                 </li>
                                 <li className={style.form_line}>
                                     <label htmlFor="email">Email</label>
-                                    <input type="email" id="email" required/>
+                                    <input type="email" id="email" name="email" required/>
                                 </li>
                                 <li className={style.form_line}>
                                     <label htmlFor="message">Message</label>
-                                    <textarea></textarea>
+                                    <textarea name="message"></textarea>
                                 </li>
                                 <li className={style.form_line}>
                                     <button type="submit">Submit</button>
